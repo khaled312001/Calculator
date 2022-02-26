9803696ff67f763c659b91155292458f4d0413cc
khaled ahmed haggagy <58705759+khaled312001@users.noreply.github.com>
2022-02-27 01:55:24 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+
+  var screen = document.querySelector('.screen');
+  var buttons = document.querySelectorAll('.btn');
+  var clear = document.querySelector('.btn-clear');
+  var equal = document.querySelector('.btn-equal');
+  
+  buttons.forEach(function(button){
+    button.addEventListener('click', function(e){
+      var value = e.target.dataset.num;
+      screen.value += value;
+    })
+  });
+  
+  equal.addEventListener('click', function(e){
+    if(screen.value == ''){
+      screen.value = 'Please Enter a Value';
+    } else {
+      var answer = eval(screen.value);
+      screen.value = answer;
+    }
+  })
+  
+  clear.addEventListener('click', function(e){
+    screen.value = '';
+  })
